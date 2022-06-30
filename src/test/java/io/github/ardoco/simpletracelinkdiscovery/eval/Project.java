74fe4daaf33ac768b3c9f15fab7b7b4f3b736a5c
Jan Keim <jan.keim@kit.edu>
2022-06-30 15:02:43 +0200

    Update benchmark repo and reliant code parts

--- src/test/java/io/github/ardoco/simpletracelinkdiscovery/eval/Project.java
+++ src/test/java/io/github/ardoco/simpletracelinkdiscovery/eval/Project.java
@@ -3,19 +3,19 @@ package io.github.ardoco.simpletracelinkdiscovery.eval;
 
 public enum Project {
     MEDIASTORE(//
-            "./src/test/resources/benchmark/mediastore/original_model/ms.repository", //
+            "./src/test/resources/benchmark/mediastore/pcm/ms.repository", //
             "./src/test/resources/benchmark/mediastore/mediastore.txt", //
             "./src/test/resources/benchmark/mediastore/goldstandard.csv"),
     TEASTORE(//
-            "./src/test/resources/benchmark/teastore/original_model/teastore.repository", //
+            "./src/test/resources/benchmark/teastore/pcm/teastore.repository", //
             "./src/test/resources/benchmark/teastore/teastore.txt", //
             "./src/test/resources/benchmark/teastore/goldstandard.csv"),
     TEAMMATES(//
-            "./src/test/resources/benchmark/teammates/original_model/teammates.repository", //
+            "./src/test/resources/benchmark/teammates/pcm/teammates.repository", //
             "./src/test/resources/benchmark/teammates/teammates.txt", //
             "./src/test/resources/benchmark/teammates/goldstandard.csv"),
     BIGBLUEBUTTON(//
-            "./src/test/resources/benchmark/bigbluebutton/original_model/bbb.repository", //
+            "./src/test/resources/benchmark/bigbluebutton/pcm/bbb.repository", //
             "./src/test/resources/benchmark/bigbluebutton/bigbluebutton_1SentPerLine.txt", //
             "./src/test/resources/benchmark/bigbluebutton/goldstandard.csv");
 
