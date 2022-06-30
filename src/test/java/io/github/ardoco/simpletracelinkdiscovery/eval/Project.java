7902df061aa6d5c5e323c207fdf88b016187d602
Jan Keim <jan.keim@kit.edu>
2022-06-30 14:55:10 +0200

    Add benchmark repository instead of a copy

--- src/test/java/io/github/ardoco/simpletracelinkdiscovery/eval/Project.java
+++ src/test/java/io/github/ardoco/simpletracelinkdiscovery/eval/Project.java
@@ -3,21 +3,21 @@ package io.github.ardoco.simpletracelinkdiscovery.eval;
 
 public enum Project {
     MEDIASTORE(//
-            "./src/test/resources/mediastore/original_model/ms.repository", //
-            "./src/test/resources/mediastore/mediastore.txt", //
-            "./src/test/resources/mediastore/goldstandard.csv"),
+            "./src/test/resources/benchmark/mediastore/original_model/ms.repository", //
+            "./src/test/resources/benchmark/mediastore/mediastore.txt", //
+            "./src/test/resources/benchmark/mediastore/goldstandard.csv"),
     TEASTORE(//
-            "./src/test/resources/teastore/original_model/teastore.repository", //
-            "./src/test/resources/teastore/teastore.txt", //
-            "./src/test/resources/teastore/goldstandard.csv"),
+            "./src/test/resources/benchmark/teastore/original_model/teastore.repository", //
+            "./src/test/resources/benchmark/teastore/teastore.txt", //
+            "./src/test/resources/benchmark/teastore/goldstandard.csv"),
     TEAMMATES(//
-            "./src/test/resources/teammates/original_model/teammates.repository", //
-            "./src/test/resources/teammates/teammates.txt", //
-            "./src/test/resources/teammates/goldstandard.csv"),
+            "./src/test/resources/benchmark/teammates/original_model/teammates.repository", //
+            "./src/test/resources/benchmark/teammates/teammates.txt", //
+            "./src/test/resources/benchmark/teammates/goldstandard.csv"),
     BIGBLUEBUTTON(//
-            "./src/test/resources/bigbluebutton/original_model/bbb.repository", //
-            "./src/test/resources/bigbluebutton/bigbluebutton.txt", //
-            "./src/test/resources/bigbluebutton/goldstandard.csv");
+            "./src/test/resources/benchmark/bigbluebutton/original_model/bbb.repository", //
+            "./src/test/resources/benchmark/bigbluebutton/bigbluebutton_1SentPerLine.txt", //
+            "./src/test/resources/benchmark/bigbluebutton/goldstandard.csv");
 
     private final String model;
     private final String textFile;
